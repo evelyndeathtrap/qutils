87c82132687c08625126de5a6296a9b0cf586cb4
Evelyn C. <evelyn.death.trap@gmail.com>
2026-08-11 23:06:48 +0300

    Create talk.c to write user input to /dev/random
    Implement a program that reads input and writes to /dev/random.

---
+++ talk.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(void) {
+
+        FILE* fp = fopen("/dev/random", "a");
+        char* ln = NULL;
+        size_t n = 4096;
+
+        printf("> ");
+        fflush(stdout); 
+        while(n = getline(&ln, &n, stdin)) {
+        for (int i = 0; i < n; i++) {
+                fwrite(&ln[i], 1, 1, fp);
+                fflush(fp);
+                sleep(1);
+        }
+        printf("> ");
+        fflush(stdout);
+        }
+} 
